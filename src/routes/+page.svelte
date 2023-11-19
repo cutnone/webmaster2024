92179625d168d07873e45f65a8c532af6a92e1a6
Noah Smith <cutspliceandplay@gmail.com>
2023-11-19 14:28:08 -0500

    initial

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,5 @@
-<script>
-    let count=0;
-    function increment(){
-        count++;
-    }
+<script lang="ts">
+    import FancyBackground from "./FancyBackground.svelte";
+
 </script>
-<h1>Welcome to SvelteKit, Kemh</h1>
-<span>{count}</span>
-<button on:click={increment}>increase</button>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<FancyBackground/>
