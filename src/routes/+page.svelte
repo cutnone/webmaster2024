6e4099e59b58b465e90b7037f545e12dbd200ddd
Kemith <kemithperera1@gmail.com>
2023-11-18 20:00:05 -0500

    Added Counter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit, dipshit.</h1>
+<script>
+    let count=0;
+    function increment(){
+        count++;
+    }
+</script>
+<h1>Welcome to SvelteKit</h1>
+<span>{count}</span>
+<button on:click={increment}>increase</button>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
