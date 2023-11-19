5bbbab095683095c2229a4053356fb168f81a718
Kemith <kemithperera1@gmail.com>
2023-11-18 20:04:33 -0500

    Add Kemith

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
         count++;
     }
 </script>
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to SvelteKit, Kemith</h1>
 <span>{count}</span>
 <button on:click={increment}>increase</button>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
