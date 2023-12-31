62aee289a54cca51edae5ec871cffe54776fbd91
Kemith <kemithperera1@gmail.com>
2023-12-31 15:44:06 -0500

    Styled Button on Homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,30 @@
+<h1>Power Your Home Through Sustainable Energy</h1>
+
 <a href="#">
     <button>Learn More</button>
 </a>
 
 
 <style>
-    
+    :global(body){
+        background-color:navy;
+    }
+    a{
+        text-decoration:none;
+        margin-top:25px;
+        display:flex;
+        justify-content:center;
+    }
+    button{
+        color:white;
+        border: 3px solid #79FF8F;
+        font-size:20px;
+        border-radius: 20px;
+        background-color:rgba(255,255,255,0.25);
+        padding: 10px 5px;
+    }
+    h1{
+        text-align:center;
+        margin-top:35vh;
+    }
 </style>
