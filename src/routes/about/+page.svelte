8df96ff6535fc7cd35c91e8893e60031167e8039
noahrkim <noah.kim414@gmail.com>
2024-01-07 18:54:33 -0500

    about update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,12 @@
 <script>
 </script>
-<main></main>
-<style></style>
+<main>
+    <h1>About</h1>
+</main>
+<style>
+    main{
+        background-image: url("images/about.jpg");
+        height:100dvh;
+        background-size: cover;
+    }
+</style>
