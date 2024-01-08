6abea2722e8f5c01439797cc04f5a77ae6c5f21b
Kemith <kemithperera1@gmail.com>
2024-01-08 09:56:55 -0500

    added content to energy sources

---
+++ src/routes/energysources/+page.svelte
@@ -0,0 +1,11 @@
+<main>
+    <section>
+        <h3>Hydroelectric Energy</h3>
+        <p>Hydroelectric power on a smaller scale, in households and communities, 
+            allows for sustainable electricity generation while avoiding the social 
+            and environmental harm that can be caused by large dams. Additionally, 
+            household hydroelectric generators can utilize creeks or rivers near the
+            property to constantly generate electricity.
+        </p>
+    </section>
+</main>
