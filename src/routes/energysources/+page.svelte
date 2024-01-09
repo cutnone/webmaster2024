befff098c459d1259c6aceaccb00fafd34ce9c5d
Kemith <kemithperera1@gmail.com>
2024-01-09 18:13:11 -0500

    did some really cool stuff on energy page

--- src/routes/energysources/+page.svelte
+++ src/routes/energysources/+page.svelte
@@ -1,97 +1,123 @@
+<script>
+    let hydro;
+    let solar;
+    let wind;
+    let bio;
+    let geothermal;
+    let money;
+    let active = "";
+</script>
 <main>
-    <section>
-        <h3>Hydroelectric Energy</h3>
-        <p>Hydroelectric power on a smaller scale, in households and communities, 
-            allows for sustainable electricity generation while avoiding the social 
-            and environmental harm that can be caused by large dams. Additionally, 
-            household hydroelectric generators can utilize creeks or rivers near the
-            property to constantly generate electricity.
-        </p>
+    <section class="hydro" class:open={active==="hydro"} bind:this={hydro} on:click={()=>{active = "hydro"}}>
+        <div>
+            <h3>Hydroelectric Energy</h3>
+            <p>Hydroelectric power on a smaller scale, in households and communities, 
+                allows for sustainable electricity generation while avoiding the social 
+                and environmental harm that can be caused by large dams. Additionally, 
+                household hydroelectric generators can utilize creeks or rivers near the
+                property to constantly generate electricity.
+            </p>
+        </div>
+        
     </section>
 
-    <section>
-        <h3>Solar Power</h3>
-        <p>
-            Solar panels convert sunlight into energy. This energy can be used to 
-            heat water and provide all the energy for a home depending on the amount
-            of panels used. Solar panels are designed to last and they can be grouped
-            together in a fixed position or attached to a tracking device to change
-            the angle of the solar panels depending on the sun’s position in the
-            sky. Solar panels for households are becoming increasingly affordable.
-            Taxpayers can claim a 30% tax credit on the cost of solar systems on
-            their income taxes. Although upfront costs can be relatively expensive,
-            usage of solar power over time can save money and increase the value of
-            a home. Additionally, solar power can be used in a wide variety of biomes
-            and climates and can generate sufficient electricity even in the winter, 
-            when there is less sunlight. Solar heating can make 50% of the hot water 
-            for a family of 4 and costs between $5,000-$7,000 dollars for installation. 
-            Solar panels cost $3 per watt installed. Installation of a 7000 watt system 
-            would cost $21,000 but produce enough electricity to meet most demands for the energy.
-
-        </p>
+    <section class="solar" class:open={active==="solar"} bind:this={solar} on:click={()=>{active = "solar"}}>
+        <div>
+            <h3>Solar Power</h3>
+            <p>
+                Solar panels convert sunlight into energy. This energy can be used to 
+                heat water and provide all the energy for a home depending on the amount
+                of panels used. Solar panels are designed to last and they can be grouped
+                together in a fixed position or attached to a tracking device to change
+                the angle of the solar panels depending on the sun’s position in the
+                sky. Solar panels for households are becoming increasingly affordable.
+                Taxpayers can claim a 30% tax credit on the cost of solar systems on
+                their income taxes. Although upfront costs can be relatively expensive,
+                usage of solar power over time can save money and increase the value of
+                a home. Additionally, solar power can be used in a wide variety of biomes
+                and climates and can generate sufficient electricity even in the winter, 
+                when there is less sunlight. Solar heating can make 50% of the hot water 
+                for a family of 4 and costs between $5,000-$7,000 dollars for installation. 
+                Solar panels cost $3 per watt installed. Installation of a 7000 watt system 
+                would cost $21,000 but produce enough electricity to meet most demands for the energy.
+    
+            </p>
+        </div>
+        
     </section>
 
-    <section>
-        <h3>Grid-connected Wind energy</h3>
-        <p>
-            Household wind energy systems that can be connected to an electricity distribution 
-            system are called grid-connected wind electric systems. Wind turbines work by using 
-            air resistance to spin a propeller, which spins a generator to make electricity. 
-            These wind turbines are used in grid-connected wind electric systems, which can be 
-            very effective in areas where average annual wind speed is at least 10 miles per hour. 
-            They can also be combined with solar and battery systems. The use of household wind 
-            energy systems can save money in the long run and allow for a much smaller carbon footprint. 
-            However, owners need at least one acre of land to install a windmill on their property. 
-            Despite these requirements, wind systems are a great investment because with proper
-            installation and maintenance, a home wind energy system can last up to 20 years or longer. 
-            This means savings for a long amount of time. Small turbines can produce around $3000-$5000 for 
-            every kilowatt of electricity produced. Most wind turbines produce around 5-15 kilowatts of energy, 
-            making costs range from $15,000 to $75,000. Costs also increase as size and height of the wind turbines increase.
-
-        </p>
+    <section class="wind" class:open={active==="wind"} bind:this={wind} on:click={()=>{active = "wind"}}>
+        <div>
+            <h3>Grid-connected Wind energy</h3>
+            <p>
+                Household wind energy systems that can be connected to an electricity distribution 
+                system are called grid-connected wind electric systems. Wind turbines work by using 
+                air resistance to spin a propeller, which spins a generator to make electricity. 
+                These wind turbines are used in grid-connected wind electric systems, which can be 
+                very effective in areas where average annual wind speed is at least 10 miles per hour. 
+                They can also be combined with solar and battery systems. The use of household wind 
+                energy systems can save money in the long run and allow for a much smaller carbon footprint. 
+                However, owners need at least one acre of land to install a windmill on their property. 
+                Despite these requirements, wind systems are a great investment because with proper
+                installation and maintenance, a home wind energy system can last up to 20 years or longer. 
+                This means savings for a long amount of time. Small turbines can produce around $3000-$5000 for 
+                every kilowatt of electricity produced. Most wind turbines produce around 5-15 kilowatts of energy, 
+                making costs range from $15,000 to $75,000. Costs also increase as size and height of the wind turbines increase.
+    
+            </p>
+        </div>
+        
     </section>
 
-    <section>
-        <h3>Geothermal Energy</h3>
-        <p>
-            Household geothermal energy units are not used to generate electricity. Instead, these systems regulate 
-            the temperature in your home by harnessing the air underground. In the winter, a geothermal home unit 
-            extracts the heat from this air and transfers it to your house in order to increase its temperature. 
-            In the summer, the home unit cools the house by moving hot air from the house underground. They have 
-            the potential to last longer and are better at heating than air pump systems. This heating system is 
-            more efficient, but it costs more than air heating pumps. Another problem with this system is that the 
-            equipment is installed underground, which makes it harder for repairs (even though repairs are very rare).
-            Even though it costs around $24,500 on average, it is more effective for heating and cooling compared to 
-            air pumps and keeps your home comfortable for less energy.
-
-        </p>
+    <section class="geothermal" class:open={active==="geothermal"} bind:this={geothermal} on:click={()=>{active = "geothermal"}}>
+        <div>
+            <h3>Geothermal Energy</h3>
+            <p>
+                Household geothermal energy units are not used to generate electricity. Instead, these systems regulate 
+                the temperature in your home by harnessing the air underground. In the winter, a geothermal home unit 
+                extracts the heat from this air and transfers it to your house in order to increase its temperature. 
+                In the summer, the home unit cools the house by moving hot air from the house underground. They have 
+                the potential to last longer and are better at heating than air pump systems. This heating system is 
+                more efficient, but it costs more than air heating pumps. Another problem with this system is that the 
+                equipment is installed underground, which makes it harder for repairs (even though repairs are very rare).
+                Even though it costs around $24,500 on average, it is more effective for heating and cooling compared to 
+                air pumps and keeps your home comfortable for less energy.
+    
+            </p>
+        </div>
+        
     </section>
 
-    <section>
-        <h3>Biomass</h3>
-        <p>
-            Biomass heating systems burn organic materials, such as wood pellets, chips, or logs to provide heating 
-            for homes and water. Compared to fossil fuels like coal and oil, this system has a very low carbon footprint, 
-            as the wood burned only emits as much carbon as the tree has absorbed. Furthermore, it's estimated that a 
-            biomass system can save up to $1100 a year compared to a traditional electric heating system.
-
-        </p>
+    <section class="bio" class:open={active==="bio"} bind:this={bio} on:click={()=>{active = "bio"}}>
+        <div>
+            <h3>Biomass</h3>
+            <p>
+                Biomass heating systems burn organic materials, such as wood pellets, chips, or logs to provide heating 
+                for homes and water. Compared to fossil fuels like coal and oil, this system has a very low carbon footprint, 
+                as the wood burned only emits as much carbon as the tree has absorbed. Furthermore, it's estimated that a 
+                biomass system can save up to $1100 a year compared to a traditional electric heating system.
+    
+            </p>
+            
+        </div>
     </section>
 
-    <section>
-        <h3>Additional Financial Bonuses</h3>
-        <p>
-            The IRS offers a 30% tax credit for installing these systems, which can be used to offset the costs of 
-            installing these systems. You are required to fill out an IRS form in order to claim this tax credit, 
-            but the government offers it to everyone who installs these systems. Certain states also have a 
-            policy called net-metering in which homeowners receive compensation for sending any excess 
-            electricity that they generate to the grid.
-
-        </p>
+    <section class="open money" class:open={active==="money"} bind:this={money} on:click={()=>{active = "money"}}>
+        <div>
+            <h3>Additional Financial Bonuses</h3>
+            <p>
+                The IRS offers a 30% tax credit for installing these systems, which can be used to offset the costs of 
+                installing these systems. You are required to fill out an IRS form in order to claim this tax credit, 
+                but the government offers it to everyone who installs these systems. Certain states also have a 
+                policy called net-metering in which homeowners receive compensation for sending any excess 
+                electricity that they generate to the grid.
+    
+            </p>
+        </div>
     </section>
 </main>
 
-<style>
+<style lang="scss">
     main{
      
         height:auto;
@@ -101,23 +127,81 @@
         color:white;
         display: flex;
         
-        flex-direction: column;
         align-items: center;
         justify-content:center;
     }
     p{
         width:500px;
+        transition: opacity .5s cubic-bezier(0, 0, 0, 1);
     }
     section{
-        background-image: url("images/hydropower.png");
-        width:100vw;
+        flex-basis:0;
+        background-position:center;
+        transition: flex-grow .5s cubic-bezier(0, 0, 0, 1);
+        min-width:0;
         height: 100vh;
         display:flex;
         flex-direction:column;
         justify-content:center;
         align-items:center;
+        flex-grow:1;
+        overflow-x:hidden;
+        overflow-y:hidden;
+        flex-shrink:1;
+
+        h3{
+            transition: rotate .5s cubic-bezier(0, 0, 0, 1);
+            white-space: nowrap;
+            font-size: 2rem;
+            text-align:center;
+            text-shadow:2px 2px 2px black;
+        }
+
+        div{
+            transition: translate .5s cubic-bezier(0, 0, 0, 1);
+        }
+
         /* background-image: url("images/hydropower.png"); */
     }
+    section.open{
+        flex-grow:10;
+     
+    }
+    section:not(.open){
+        cursor:pointer;
+        h3 {
+            rotate:-90deg;
+        }
+        p {
+            opacity:0;
+        }
+        &:hover {
+            flex-grow:2; 
+        }
+        div{
+            translate:0 50%;
+        }
+    }
+
+    section.hydro{
+        background-image:url("images/hydropower.png");
+    }
+    section.solar{
+        background-image:url("images/solar-cells.png");
+    }
+    section.bio{
+        background-image:url("images/biomass.jpg");
+    }
+    section.wind{
+        background-image:url("images/wind-energy.png");
+    }
+    section.geothermal{
+        background-image:url("images/geothermal.jpg");
+    }
+    section.money{
+        background-image:url("images/money.jpg");
+    }
+
 
     
 </style>
