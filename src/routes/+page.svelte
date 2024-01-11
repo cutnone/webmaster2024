b00c0f04855186cbb6ce57ea902ecc1124c4d5e1
Kemith <kemithperera1@gmail.com>
2024-01-10 19:32:57 -0500

    added logo image to folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,14 +22,26 @@
     }
     button{
         color:white;
-        border: 3px solid #79FF8F;
+        outline: 3px solid #79FF8F;
         font-size:20px;
         border-radius: 20px;
         background-color:rgba(255,255,255,0.25);
         padding: 15px 30px;
         backdrop-filter: blur(5px);
         box-shadow: 0 0 10px raba(0 0 0 50%);
+        border:none;
         cursor: pointer;
+        outline-offset:0;
+        transition: none .5s cubic-bezier(0, 0, 0, 1);
+        transition-property:outline-offset, scale;
+    }
+    button:hover{
+        outline: 3px solid #79FF8F;
+        outline-offset:5px;
+        cursor:pointer;
+    }
+    button:active {
+        scale: 0.9;
     }
     h1{
         text-shadow: 0 0 10px raba(0 0 0 50%);
