421a9a5bd167d071af396138bb1fec0739e98c94
Noah Smith <cutspliceandplay@gmail.com>
2024-01-13 19:29:57 -0500

    start ref page

--- src/routes/references/+page.svelte
+++ src/routes/references/+page.svelte
@@ -1,19 +1,52 @@
 <main>
-    <a href='https://drive.google.com/file/d/1KU-nE0jrawxqkuOXUnwKEZezH2jyuHgx/view' target="_blank">Plan of Work Log</a>
-    <a href='https://drive.google.com/file/d/1F-q-5dDDGP3uel-kC8htiAVl3SUOypXZ/view' target="_blank">Works Cited</a>
-    <a href="">Copyright Checklist</a>
+    <h1>References</h1>
+    <div class="links">
+        <a href='https://drive.google.com/file/d/1KU-nE0jrawxqkuOXUnwKEZezH2jyuHgx/view' target="_blank">Plan of Work Log</a>
+        <a href='https://drive.google.com/file/d/1F-q-5dDDGP3uel-kC8htiAVl3SUOypXZ/view' target="_blank">Works Cited</a>
+        <a href="">Copyright Checklist</a>
+    </div>
 </main>
 
 <style>
     a{
         color:white;
         text-decoration:none;
-        margin-top:50px;
-        display:flex;
-        justify-content:center;
+        display: grid;
+        place-items: center;
+        /* background-color: #005500; */
+        padding: 1em 2em;
+        background-size: 50%;
+    }
+
+    h1 {
+        color: white;
     }
 
     a:hover{
         text-decoration:underline;
+    }
+
+    .links {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 3em;
+    }
+
+    main {
+        height: 100dvh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 3em;
+        flex-direction: column;
+        background-image: radial-gradient(circle at center, #005500 0%,black 100%);
+
+    }
+
+    @media screen and (max-width: 700px) {
+        .links {
+            flex-direction: column;
+        }
     }
 </style>
